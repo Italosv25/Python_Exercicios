33f8e182a8c65fe354042777b1db1f189d0d0d9d
ItaloMac <italosvidigal@gmail.com>
2026-05-08 07:49:24 -0300

    adicionado resolução matematica

---
+++ somas.py
@@ -0,0 +1,10 @@
+A = (int(input("Digite um valor para A: ")))
+B = (int(input("Digite um valor para B: ")))
+Soma = A + B
+print("Soma: ", Soma)
+Divisao = A / B
+print("Divisao: ", Divisao)
+Subtracao = A - B
+print("Subtracao: ", Subtracao)
+Multiplicacao = A * B
+print("Multiplicacao: ", Multiplicacao)
